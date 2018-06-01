d54c3cfade903d5cb80905387a0d8b8f37c5a3f5
Leon Hartley <leonhartley@ekmsystems.co.uk>
2018-06-01 19:57:30 +0100

    Remove boost dependency, use std::lock_guard and other standard types

--- DummyConnection.h
+++ DummyConnection.h
@@ -14,57 +14,53 @@
  */
 
 #include "ConnectionPool.h"
-using boost::shared_ptr;
 
 namespace active911 {
 
 
-	class DummyConnection : public Connection {
+    class DummyConnection : public Connection {
 
-	public:
+    public:
 
-		DummyConnection() {
+        DummyConnection() {
 
-			_DEBUG("Dummy connection created");
+            _DEBUG("Dummy connection created");
 
-		};
+        };
 
-		~DummyConnection() {
+        ~DummyConnection() {
 
-			// Destroy the connection
+            // Destroy the connection
 
-			_DEBUG("Dummy connection destroyed");
+            _DEBUG("Dummy connection destroyed");
 
-		};
+        };
 
-		// shared_ptr to some kind of actual connection object would go here
+        // shared_ptr to some kind of actual connection object would go here
 
-	};
-
-
-	class DummyConnectionFactory : public ConnectionFactory {
-
-	public:
-		DummyConnectionFactory() {
+    };
 
 
-		};
+    class DummyConnectionFactory : public ConnectionFactory {
 
-		// Any exceptions thrown here should be caught elsewhere
-		shared_ptr<Connection> create() {
-
-			// Create the connection
-			shared_ptr<DummyConnection>conn(new DummyConnection());
-
-			// Perform some kind of ->connect operation here
-
-			return boost::static_pointer_cast<Connection>(conn);
-		};
-
-	};
+    public:
+        DummyConnectionFactory() {
 
 
+        };
 
+        // Any exceptions thrown here should be caught elsewhere
+        std::shared_ptr<Connection> create() {
+
+            // Create the connection
+            std::shared_ptr<DummyConnection> conn(new DummyConnection());
+
+            // Perform some kind of ->connect operation here
+
+            return std::static_pointer_cast<Connection>(conn);
+        };
+
+    };
 
 
 }
